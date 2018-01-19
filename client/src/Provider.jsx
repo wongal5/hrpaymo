b3802d8d2ebe2b8c319781f654d3951fc837d383
wongal5 <a.wong9970@gmail.com>
2018-01-18 18:42:19 -0800

    removed unnecessary packages

--- client/src/Provider.jsx
+++ client/src/Provider.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
 import App from './App.jsx';
 import paymo from './components/Reducers';
 
-let storeCreator = applyMiddleware(reduxPromise)(createStore);
-let store = storeCreator(paymo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+let store = createStore(paymo, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
