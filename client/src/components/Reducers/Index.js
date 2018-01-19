95e09cb61420ab3ce92bf3bdec15944ed7ba540b
Larry <lchang2190@gmail.com>
2018-01-19 14:36:27 -0800

    changed initial state in reducer

--- client/src/components/Reducers/Index.js
+++ client/src/components/Reducers/Index.js
@@ -17,7 +17,7 @@ import { LOG_USER_OUT,
                 } from './actions';
 import axios from 'axios';
 
-function paymo(state = {
+const initialState = {
     isLoggedIn: false,
     globalFeed: {},
     userFeed: {},
@@ -32,7 +32,10 @@ function paymo(state = {
     profileInfo: {},
     unknownUser: false,
     profileFeed: {},
-    relationalFeed: {}}, action) {
+    relationalFeed: {}
+}
+
+function paymo(state = initialState, action) {
     // console.log('paymo reducer was called with state', state, 'and action', action)
     switch (action.type) {
         case LOG_USER_OUT:
@@ -94,7 +97,7 @@ function paymo(state = {
               [action.payload.name]: action.payload.value
           })
         case PROFILE_LOAD_MORE_FEED:
-            console.log('reducer load more feed profile', action.payload)
+            // console.log('reducer load more feed profile', action.payload)
             return Object.assign({}, state, {
                 [action.payload.feedType]: action.payload.obj
             })
